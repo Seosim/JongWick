74869e0fd0734751470fb89f427b27dcbbe77f6b
Seosim <103580344+Seosim@users.noreply.github.com>
2024-08-16 21:25:28 +0900

    적 무기 추가

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -7,13 +7,16 @@ using UnityEngine.UI;
 public class Enemy : MonoBehaviour
 {
     public Action aHit;
+    public Action aAttack;
 
     public float DetectingRange = 0.0f;
 
-    public float MoveDelay = 1.0f;
+    public float MoveDelay = 3.0f;
+    public float MoveTime = 1.0f;
     public float Speed = 5.0f;
-
     public float MaxHp = 10.0f;
+
+    public bool bAttack { get { return !mbWalk; } private set { } }
 
     private Rigidbody2D mRigidBody;
     private Animator mAnimator;
@@ -21,9 +24,10 @@ public class Enemy : MonoBehaviour
     private SpriteRenderer mSpriteRenderer;
     private Vector3 mDirection = Vector2.zero;
     private float mMoveDelay = 0.0f;
+    private float mMoveTime = 0.0f;
     private float mHp = 0.0f;
 
-    private bool mWalk = false;
+    private bool mbWalk = false;
 
     // Start is called before the first frame update
     void Start()
@@ -33,13 +37,13 @@ public class Enemy : MonoBehaviour
         mSpriteRenderer = GetComponent<SpriteRenderer>();
         mBTEnemy = new BTEnemy(initializeBT());
 
-        mMoveDelay = MoveDelay;
+        mMoveTime = MoveTime;
         mHp = MaxHp;
     }
 
     private void FixedUpdate()
     {
-        if(mWalk)
+        if(mbWalk)
             mRigidBody.MovePosition(transform.position + mDirection * Speed * Time.fixedDeltaTime);
     }
 
@@ -96,8 +100,9 @@ public class Enemy : MonoBehaviour
 
     private INode.State AttackPlayer()
     {
-        mWalk = false;
-        mAnimator.SetBool("Move", mWalk);
+        aAttack?.Invoke();
+        mbWalk = false;
+        mAnimator.SetBool("Move", mbWalk);
         return INode.State.Success;
     }
     #endregion
@@ -105,26 +110,36 @@ public class Enemy : MonoBehaviour
 #region Move
     private INode.State SetMoveDirection()
     {
-        if(mMoveDelay == MoveDelay)
+        if(mMoveDelay >= 0.0f)
+        {
+            mMoveDelay -= Time.deltaTime;
+            mDirection = Vector2.zero;
+            mbWalk = false;
+            mAnimator.SetBool("Move", mbWalk);
+            return INode.State.Running;
+        }
+
+        if (mMoveTime == MoveTime)
         {
             mDirection = UnityEngine.Random.insideUnitCircle.normalized;
-            mWalk = true;
+            mbWalk = true;
             mSpriteRenderer.flipX = mDirection.x < 0;
-            mAnimator.SetBool("Move", mWalk);
+            mAnimator.SetBool("Move", mbWalk);
         }
         return INode.State.Success;
     }
 
     private INode.State WalkAround()
     {
-        if(mMoveDelay <= 0.0f)
+        if(mMoveTime <= 0.0f)
         {
+            mMoveTime = MoveTime;
             mMoveDelay = MoveDelay;
             return INode.State.Success;
         }
         else
         {
-            mMoveDelay -= Time.deltaTime;
+            mMoveTime -= Time.deltaTime;
             return INode.State.Running;
         }
     }
---
+++ Assets/Scripts/Weapon/WeaponAI.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponAI : MonoBehaviour
+{
+    [SerializeField] private GameObject m_Bullet;
+    public float MaxRecoil = 0.1f;
+    public float Recoil = 0.0f;
+    public float RecoilRecovery = 1.0f;
+    public float FireRate = 1.0f;
+
+    private SpriteRenderer mSpriteRenderer;
+    private Vector2 mDirection;
+    private List<GameObject> mAmmo;
+    private int mRemainAmmoCount;
+
+    private float mReloadTime = 0.0f;
+    private float mRecoil = 0.0f;
+    private float mFireRate = 0.0f;
+
+    private bool mbAttack = false;
+
+    private Enemy mEnemy;
+
+    private void Start()
+    {
+        mSpriteRenderer = GetComponent<SpriteRenderer>();
+        mEnemy = GetComponentInParent<Enemy>();
+        mEnemy.aAttack += Shoot;
+
+        mAmmo = new List<GameObject>();
+        //TODO: 총알 처리 필요
+        mRemainAmmoCount = 15;
+        for (int i = 0; i < 15; i++)
+        {
+            GameObject obj = Instantiate(m_Bullet);
+            obj.SetActive(false);
+            mAmmo.Add(obj);
+        }
+    }
+
+    void Update()
+    {
+        //Rotate Weapon
+        {
+            if(mEnemy.bAttack)
+            {
+                Vector2 playerPosition = GameManager.gameManager.player.transform.position;
+                mDirection = new Vector2(transform.position.x - playerPosition.x, transform.position.y - playerPosition.y);
+
+                float angle = Mathf.Atan2(mDirection.x, -mDirection.y) * Mathf.Rad2Deg;
+                mSpriteRenderer.flipY = angle > 0.0f;
+                angle += 90.0f;
+                transform.rotation = Quaternion.Euler(0.0f, 0.0f, angle);
+            }
+
+        }
+
+        //Recoil Recovery
+        {
+            mRecoil = Mathf.Lerp(mRecoil, 0.0f, Time.deltaTime * RecoilRecovery);
+        }
+        //Fire Rate
+        {
+            if (mFireRate > 0.0f)
+            {
+                mFireRate = Mathf.Max(mFireRate - Time.deltaTime, 0.0f);
+            }
+        }
+    }
+
+    private void Shoot()
+    {
+        if (mFireRate > 0.0f)
+            return;
+
+        foreach (GameObject obj in mAmmo)
+        {
+            if (!obj.activeSelf)
+            {
+                obj.SetActive(true);
+                Bullet bullet = obj.GetComponent<Bullet>();
+
+                Vector2 direction = -mDirection.normalized + new Vector2(UnityEngine.Random.Range(-mRecoil, mRecoil), UnityEngine.Random.Range(-mRecoil, mRecoil));
+                bullet.Initialize(transform.position + transform.right, direction.normalized, transform.rotation);
+                --mRemainAmmoCount;
+                mRecoil = Mathf.Min(MaxRecoil, mRecoil + Recoil);
+                mFireRate = FireRate;
+                break;
+            }
+        }
+    }
+}
